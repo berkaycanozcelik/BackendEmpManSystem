62e312143d400ef095e5e0b084187c515fe47c2c
BerkayCan Ozcelik <berkay.oezcelik@demirtag.de>
2023-06-02 16:15:16 +0300

    initial

---
+++ src/main/java/com/berkay/EMS/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.berkay.EMS.repository;
+
+import com.berkay.EMS.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
